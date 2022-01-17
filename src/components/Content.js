e0cde96ec1d53ed59f4a7b3657f6b29f0af91aec
Pochmannn <73834683+Pochmannn@users.noreply.github.com>
2022-01-17 15:43:05 +0100

    ss

---
+++ src/components/Content.js
@@ -0,0 +1,7 @@
+function Content(){
+    return(   
+        <div className="bg-gray-500 w-3/5 ml-auto h-5/6  mt-9 mr-auto  border-black rounded-lg  ">
+            <span className="text-black italic font-black  ml-auto mr-auto text-4xl">Statki oguem statystki:</span>         
+        </div>
+    )}
+export default Content;
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+
+
+function Navbar(){
+    return(
+        <navbar className="bg-gray-800  text-4xl items-center text-white w-full flex justify-left h-16  ">                   
+            <img className="h-16 ml-4" src="https://upload.wikimedia.org/wikipedia/commons/3/34/Gmod_logo.png"/>            
+            <span className="font-mono  font-black text-white" > Film Finder  </span>                  
+        </navbar>   
+        )}
+
+export default Navbar;
